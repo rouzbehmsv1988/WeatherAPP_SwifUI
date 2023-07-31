0821ac4ef98be79ee96a38ae5b14d6488eb86561
rouzbeh <rouzbehmsv1988@gmail.com>
2023-07-31 11:34:38 +0200

    Initial Commit

---
+++ WeatherAPP/WeatherAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPPApp.swift
+//  WeatherAPP
+//
+//  Created by rouzbeh on 31.07.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
